b8403432a810b030c3b827ed27c8ca037ec3235c
Roksana Khanam <roksanakhanamseo@gmail.com>
2024-08-19 02:35:49 +0600

    new

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 import { MdLocationOn } from "react-icons/md";
 
 export default function ListingItem({ listing }) {
-  const dirname = "../../backend/public/my-uploads/";
+  // const dirname = "../../backend/public/my-uploads/";
 
   return (
     <div className="bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[330px]">
       <Link to={`/listing/${listing._id}`}>
         <img
-          src={dirname + listing.imageUrls[0]}
+          src={listing.imageUrls[0]}
           alt="listing cover"
           className="h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300"
         />
